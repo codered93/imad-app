ec7f41802dc32e190393839ab61eaa2e74227b6b
codered93 <himanshujsr@yahoo.co.in>
2017-08-27 17:19:12 +0530

    XMLHttpRequest

--- server.js
+++ server.js
@@ -76,6 +76,21 @@ return htmlTempelate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+var counter=0;
+app.get('/counter',function(req,res){
+	counter=counter+1;
+	res.send(counter.toString());
+});
+
+var names=[];
+app.get('/submit-name',function(req,res){
+	var name=req.query.name;
+	names.push(name);
+	res.send(JSON.stringify(names));//to convert object to string 
+});
+
+
 app.get('/:articleName',function(req,res){
 	//articleName == article-one
 	//articles[articleName] == {} content object for article one
--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
-console.log('Loaded!');
+/*console.log('Loaded!');
 
 //Change the text of the main Div
 var element=document.getElementById('main-text');
@@ -16,4 +16,69 @@ function moveRight(){
 
 img.onclick=function(){
 	var interval=setInterval(moveRight,50);
+};*/
+
+var button=document.getElementById('counter');
+var counter=0;
+button.onclick=function(){
+	//Create a request to the counter endpoint
+	var request=new XMLHttpRequest();
+
+	//Capture the response and store it in a variable
+
+	request.onreadystatechange=function(){
+		if(request.readyState === XMLHttpRequest.DONE){
+			//Take some action
+			if(request.status === 200){
+				var counter=request.responseText;
+				var span=document.getElementById('count');
+				span.innerHTML=counter.toString();
+			}
+		}
+	};
+
+	//Make the request
+	request.open('GET','http://localhost:8090/counter',true)
+	request.send(null);
+
+
+	//Render the variable in correct span
+	/*counter=counter+1;
+	var span=document.getElementById('count');
+	span.innerHTML=counter.toString();*/
+};
+
+//Submit name
+
+var submit=document.getElementById('submit-btn');
+submit.onclick=function(){
+	//create a request object
+	var request=new XMLHttpRequest();
+
+	//Capture the response and store it in a variable
+
+	request.onreadystatechange=function(){
+		if(request.readyState === XMLHttpRequest.DONE){
+			//Take some action
+			if(request.status === 200){
+				//Capture the list of names and render as a list
+					var names=request.responseText;
+					names=JSON.parse(names);
+					var list='';
+					for(var i=0;i<names.length;i++)
+					{
+						list+='<li>'+names[i]+'</li>';
+					}
+					var ul=document.getElementById('namelist');
+					ul.innerHTML=list;
+			}
+		}
+	};
+
+	//Make the request
+	var nameInput=document.getElementById('name');
+	var name=nameInput.value;
+	request.open('GET','http://localhost:8090/submit-name?name='+name,true) //Here the name is coming from input box at the top
+	request.send(null);
+	
 };
