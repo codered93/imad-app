8cb326b757c63f5c65e06334145115396c71f1f5
codered93 <himanshujsr@yahoo.co.in>
2017-08-27 15:56:12 +0530

    moving image

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,19 @@
 console.log('Loaded!');
+
+//Change the text of the main Div
+var element=document.getElementById('main-text');
+element.innerHTML='New Value';
+
+//Move the image
+
+var img=document.getElementById('madi');
+
+var marginLeft=0;
+function moveRight(){
+	marginLeft=marginLeft+5;
+	img.style.marginLeft=marginLeft+'px';
+}
+
+img.onclick=function(){
+	var interval=setInterval(moveRight,50);
+};
